89ccba94e3d4060e76d60b369ae8f41919bd3158
Musika Iqfat <musfikaiqfatmomo21.com>
2025-10-03 01:34:07 +0600

    created layout

---
+++ src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NonDashboardNavbar() {
+  return (
+    <div>
+      <nav className='nonDashboard-layout'>
+        hello
+      </nav>
+    </div>
+  )
+}
