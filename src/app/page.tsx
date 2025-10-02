09ad657846251f975e41654eb6d2fd8f2c7fe0eb
Musika Iqfat <musfikaiqfatmomo21.com>
2025-10-03 03:07:26 +0600

    created navigation menu in non dashboard part and created skeletons for landing pages banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "./(nondashboard)/landing/page";
 
 export default function Home() {
   return (
     <div className="nonDashboard-layout">
       <NonDashboardNavbar/>
+      <main>
+        <Landing/>
+      </main>
     </div>
   );
 }
