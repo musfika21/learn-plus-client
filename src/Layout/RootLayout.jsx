0589b9319b2974731fba524de8f62b87ac3b82a9
003-demo <rrriku05@gmail.com>
2025-10-10 00:44:55 +0600

    banner

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Home from "../pages/home/home";
+import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div className="text-2xl">
+    <div className="max-w-screen-xl mx-auto">
       <Navbar></Navbar>
-      <Home></Home>
+      <Outlet></Outlet>
     </div>
   );
 };
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,46 +1,34 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // ✅ Import default styles
+import { Carousel } from "react-responsive-carousel";
 import img1 from "../../../assets/img1.png";
 import img2 from "../../../assets/img2.png";
 import img3 from "../../../assets/img3.png";
 
 const Banner = () => {
   return (
-    <div className="w-full h-screen rounded-none overflow-hidden shadow-lg">
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        loop
-        className="w-full h-full"
+    <div className="m-1 shadow-xl shadow-primary/50 rounded-2xl overflow-hidden">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        interval={3000}
+        stopOnHover
+        swipeable
+        emulateTouch
+        transitionTime={1000}
       >
-        <SwiperSlide>
-          <img
-            src={img1}
-            alt="Slide 1"
-            className="w-full h-full object-cover"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={img2}
-            alt="Slide 2"
-            className="w-full h-full object-cover"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={img3}
-            alt="Slide 3"
-            className="w-full h-full object-cover"
-          />
-        </SwiperSlide>
-      </Swiper>
+        <div>
+          <img src={img1} alt="Slide 1" className="rounded-2xl" />
+        </div>
+        <div>
+          <img src={img2} alt="Slide 2" className="rounded-2xl" />
+        </div>
+        <div>
+          <img src={img3} alt="Slide 3" className="rounded-2xl" />
+        </div>
+      </Carousel>
     </div>
   );
 };
